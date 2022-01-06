600475349e09a1dde836610ee7f690b6ffd618cf
zhanghaojie <zhj778930960@outlook.com>
2022-01-06 23:33:46 +0800

    <feat> builds entity

--- src/modules/builds/entities/build.entity.ts
+++ src/modules/builds/entities/build.entity.ts
@@ -3,6 +3,81 @@ import { Column, Entity } from "typeorm";
 
 @Entity('builds')
 export class Build  extends BaseEntity{
+
+  @Column({
+    name: 'task_id',
+    type: 'int',
+    nullable: false
+  })
+  taskId: number;
+
+  @Column({
+    name: 'status',
+    type: 'int',
+    nullable: false
+  })
+  status: number;
+
+  @Column({
+    name: 'user_id',
+    type: 'int',
+    nullable: false
+  })
+  userId: number;
+
+  @Column({
+    name: 'job_name',
+    type: 'varchar',
+    length: 255,
+    nullable: false
+  })
+  jobName: string;
+
+  @Column({
+    name: 'number',
+    type: 'int',
+    nullable: false
+  })
+  number: number;
+
+  @Column({
+    name: 'parameters',
+    type: 'json',
+    nullable: true
+  })
+  parameters: unknown;
+
+  @Column({
+    name: 'build_type',
+    type: 'varchar',
+    length: 255,
+    nullable: false
+  })
+  buildType: string;
+
+  /**
+   * @description  mongodb document id
+   */
+  @Column({
+    name: 'file_id',
+    type: 'varchar',
+    length: 255,
+    nullable: true
+  })
+  fileId: string;
+
+  /**
+   * @description  build result  id 文件数据提取后的id
+   */
+
+  @Column({
+    name: 'result_id',
+    type: 'int',
+    nullable: true
+  })
+  resultId: number;
+
+
   @Column({
     name: 'project_id',
     type: 'int',
